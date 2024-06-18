2052626d8231352144fe2533876935c38ea054a1
mariantoniettaangelini <164540376+mariantoniettaangelini@users.noreply.github.com>
2024-06-18 14:22:29 +0200

    Aggiungere i file di progetto.

---
+++ Esercitazione/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercitazione
+{
+    internal class persona
+    {
+        string nome;
+        string cognome;
+        int eta;
+
+        public string Nome
+        {
+            get { return nome; } set { nome = value; }
+        }
+        public string Cognome
+        {
+            get { return cognome; } set { cognome = value; }
+        }
+        public int Eta
+        {
+            get { return eta; } set { eta = value; }
+        }
+
+        public void getNome()
+        {
+            Console.WriteLine(nome);
+        }
+        public void getCognome()
+        {
+            Console.WriteLine(cognome);
+        }
+        public void getDettagli()
+        {
+            Console.WriteLine("Mi chiamo" + " " + nome + " " + cognome + " " + "e ho anni" + " " + eta);
+        }
+    }
+}
---
+++ Esercitazione/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Esercitazione
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            persona a = new persona();
+            a.Nome = "Mario";
+            a.Cognome = "Rossi";
+            a.Eta = 30;
+            a.getNome();
+            a.getCognome();
+            a.getDettagli();
+
+            persona b = new persona();
+            b.Nome = "Luigi";
+            b.Cognome = "Bianchi";
+            b.Eta = 20;
+            b.getNome();
+            b.getCognome();
+            b.getDettagli();
+
+        }
+    }
+}
